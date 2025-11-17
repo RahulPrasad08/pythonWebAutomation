115aceb9bb3cc7fe78a22855a145719b93e2028f
Dell@DESKTOP-A8LEIQL <sons.rahul@gmail.com>
2025-11-17 12:12:16 +0530

    Add Demo4Project

---
+++ Demo4.py
@@ -0,0 +1,8 @@
+City = "Ranchi"
+print("My Native town is " + City)
+
+state = "Ranchi is the {0} {1} {2}".format("capital", "of", "jharkhand")
+print(state)
+
+
+print(f"My Native town is {City}" )
