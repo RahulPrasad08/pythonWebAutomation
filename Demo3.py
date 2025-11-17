2318f8c6984c075df6a5880af7e3a8eca59f09ab
Dell@DESKTOP-A8LEIQL <sons.rahul@gmail.com>
2025-11-17 11:21:49 +0530

    Type message of your choice here

---
+++ Demo3.py
@@ -0,0 +1,13 @@
+from operator import index
+
+city = "Mu Home town is Ranchi"
+print(len(city))
+print(city.index('t'))
+print(city.replace('u','y'))
+print(city.split('H'))
+print(city.split(" "))
+print(city.upper())
+print(city.lower())
+print(city.title())
+print(city.count('o'))
+
