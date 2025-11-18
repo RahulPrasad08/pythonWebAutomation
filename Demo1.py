ebe978c891fd0a6a456f5d70c54ace5bbc967826
Dell@DESKTOP-A8LEIQL <sons.rahul@gmail.com>
2025-11-18 14:13:26 +0530

    Your descriptive message

--- Demo1.py
+++ Demo1.py
@@ -19,4 +19,9 @@ lname = 'Prasad'
 print( fname + lname)
 
 print("\"Rahul\"")
+print("################################")
+name = "I live in Navi\tMumbai"
+print(name)
+city = "Mumbai\nMaharastra"
+print(city)
 
